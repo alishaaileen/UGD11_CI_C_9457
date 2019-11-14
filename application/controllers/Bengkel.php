687313c9d9c89eef45b4278b5cd829b1be628dad
Alisha Aileen <alishaileen05@gmail.com>
2019-11-14 12:21:50 +0700

    initial commit

---
+++ application/controllers/Bengkel.php
@@ -0,0 +1,65 @@
+<?php
+use Restserver \Libraries\REST_Controller ;
+Class Bengkel extends REST_Controller{
+    public function __construct(){
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding");
+        parent::__construct();
+        $this->load->model('BengkelModel');
+        $this->load->library('form_validation');
+    }
+    public function index_get(){
+        return $this->returnData($this->db->get('branches')->result(), false);
+    }
+    public function index_post($id = null){
+        $validation = $this->form_validation;
+        $rule = $this->BengkelModel->rules();
+		array_push($rule,
+			[
+				'field' => 'name',
+				'label' => 'name',
+				'rules' => 'required|alpha'
+			],
+			[
+				'field' => 'phoneNumber',
+				'label' => 'phoneNumber',
+				'rules' => 'required|is_unique[branches.phoneNumber]'
+			]
+		);
+        $validation->set_rules($rule);
+		if (!$validation->run()) {
+			return $this->returnData($this->form_validation->error_array(), true);
+        }
+		$bengkel = new BengkelData();
+        $bengkel->name = $this->post('name');
+        $bengkel->address = $this->post('address');
+		$bengkel->phoneNumber = $this->post('phoneNumber');
+		$bengkel->created_at = $this->post('created_at');
+        if($id == null){
+            $response = $this->BengkelModel->store($bengkel);
+        }else{
+            $response = $this->BengkelModel->update($bengkel,$id);
+        }
+        return $this->returnData($response['msg'], $response['error']);
+    }
+    public function index_delete($id = null){
+        if($id == null){
+			return $this->returnData('Parameter Id Tidak Ditemukan', true);
+        }
+        $response = $this->BengkelModel->destroy($id);
+        return $this->returnData($response['msg'], $response['error']);
+    }
+    public function returnData($msg,$error){
+        $response['error']=$error;
+        $response['message']=$msg;
+        return $this->response($response);
+    }
+}
+Class BengkelData{
+	public $id;
+    public $name;
+    public $address;
+	public $phoneNumber;
+	public $created_at;
+}
---
+++ application/models/BengkelModel.php
@@ -0,0 +1,57 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class BengkelModel extends CI_Model
+{
+    private $table = 'branches';
+
+    public $id;
+    public $name;
+    public $address;
+	public $phoneNumber;
+	public $created_at;
+    public $rule = [ 
+        [
+            'field' => 'name',
+            'label' => 'name',
+            'rules' => 'required|alpha'
+		],
+		[
+			'field' => 'phoneNumber',
+			'label' => 'phoneNumber',
+			'rules' => 'required|integer|is_unique[branches.phoneNumber]'
+		]
+    ];
+    public function Rules() { return $this->rule; }
+   
+
+    public function getAll() { return 
+        $this->db->get('data_bengkel')->result(); 
+    } 
+    public function store($request) { 
+        $this->name = $request->name; 
+		$this->address = $request->address;
+		$this->phoneNumber = $request->phoneNumber; 
+        $this->created_at = $request->created_at; 
+        if($this->db->insert($this->table, $this)){
+            return ['msg'=>'Berhasil','error'=>false];
+        }
+        return ['msg'=>'Gagal','error'=>true];
+    }
+    public function update($request,$id) { 
+        $updateData = ['phoneNumber' => $request->phoneNumber, 'name' =>$request->name];
+        if($this->db->where('id',$id)->update($this->table, $updateData)){
+            return ['msg'=>'Berhasil','error'=>false];
+        }
+        return ['msg'=>'Gagal','error'=>true];
+    }
+    public function destroy($id){
+        if (empty($this->db->select('*')->where(array('id' => $id))->get($this->table)->row())) return ['msg'=>'Id tidak ditemukan','error'=>true];
+        
+        if($this->db->delete($this->table, array('id' => $id))){
+            return ['msg'=>'Berhasil','error'=>false];
+        }
+        return ['msg'=>'Gagal','error'=>true];
+    }
+}
+?>
